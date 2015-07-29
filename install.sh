684d94f60475e35afd654518c577ad298ca5968d
Kristian Zondervan <kristian.zondervan@gmail.com>
2015-07-29 14:18:12 +0200

    Add default git config

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+git config --global core.excludesfile ~/.gitignore_global
